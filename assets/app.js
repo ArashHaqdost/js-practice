482633884247a7935b8a7038410bad6b30eeaf6c
Arash <shokrullahhaqdost87@gmail.com>
2022-04-16 15:22:11 +0500

    CREATE the winner  functions

--- assets/app.js
+++ assets/app.js
@@ -1,34 +1,62 @@
-const startGamebtn=document.getElementById('start-game-btn');
+const startGamebtn = document.getElementById('start-game-btn');
 
-const ROCK='ROCK';
-const PAPER='PAPER';
-const SCISSOR='SCISSOR';
-const DIFFULT_CHOSE_VLAUE=ROCK;
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSOR = 'SCISSOR';
+const DIFFULT_CHOSE_VLAUE = ROCK;
+const DRAW = 'DRAW';
+const RESULT_COMPUTER_WIN = 'COMPUTER WIN';
+const RESULT_PLAYER_WIN = 'PLAYER WIN';
 
-let gameIsRunnig=false;
+let gameIsRunnig = false;
 
-const getPlayerChose = function(){
-    const slection=prompt(`${ROCK},${PAPER}or ${SCISSOR} ?`,'').toLocaleUpperCase();
+const getPlayerChose = function () {
+  const slection = prompt(
+    `${ROCK},${PAPER}or ${SCISSOR} ?`,
+    ''
+  ).toLocaleUpperCase();
 
-    if (
-        slection !==ROCK&&
-        slection !==PAPER&&
-        slection !==SCISSOR
-    ){
-        alert(`invalid choise! we chose ${DIFFULT_CHOSE_VLAUE} for you!`);
-        return DIFFULT_CHOSE_VLAUE;
-    }
-    return slection;
-
-
+  if (slection !== ROCK && slection !== PAPER && slection !== SCISSOR) {
+    alert(`invalid choise! we chose ${DIFFULT_CHOSE_VLAUE} for you!`);
+    return DIFFULT_CHOSE_VLAUE;
+  }
+  return slection;
 };
-
-startGamebtn.addEventListener('click',function(){
-    if (gameIsRunnig){
-        return;
-    }
-    gameIsRunnig=true;
-    console.log('Loding this Game...');
-    const playerSeclition= getPlayerChose();
-    console.log(playerSeclition);
-});//call indirct functions
+//start the random functions
+const getComputerChoise = function () {
+  const randoValue = Math.random();
+  if (randoValue < 0.34) {
+    return ROCK;
+  } else if (randoValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSOR;
+  }
+};
+//start the  Get winner functions
+const getWinner = function (cChoise, pChoise) {
+  if (cChoise === pChoise) {
+    return DRAW;
+  } else if (
+    (cChoise === ROCK && pChoise === SCISSOR) ||
+    (cChoise === PAPER && pChoise === ROCK) ||
+    (cChoise === SCISSOR && pChoise === PAPER)
+  ) {
+    return RESULT_PLAYER_WIN;
+  } else {
+    return RESULT_COMPUTER_WIN;
+  }
+};
+//start the eventListner
+startGamebtn.addEventListener('click', function () {
+  if (gameIsRunnig) {
+    return;
+  }
+  gameIsRunnig = true;
+  console.log('Loding this Game...');
+  const playerChoise = getPlayerChose();
+  const ComputerChoise = getComputerChoise();
+  const winner = getWinner(ComputerChoise, playerChoise);
+  console.log(winner); 
+});
+//call indirct functions
